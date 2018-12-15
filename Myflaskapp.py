5b03a7461c93101ffc54bfde7bdd3ac809df5162
thomashuang2017 <38603076+thomashuang2017@users.noreply.github.com>
2018-12-15 23:36:37 +0800

    try

--- Myflaskapp.py
+++ Myflaskapp.py
@@ -60,10 +60,10 @@ def register():
 @app.route('/login',methods=['GET','POST'])
 def login():
     if request.method =='POST':
-        
+
         username = request.form['username']
         password = request.form['password']
-        
+
         # username not found , password error , success login
 
         result,data = DoSQL().S_db("SELECT * FROM users WHERE username = %s",[username],1)
@@ -159,6 +159,7 @@ def exInfo(county_name):
 @app.route('/recommand',methods=['GET','POST'])
 @is_logged_in
 def render_recommand():
+<<<<<<< HEAD
     #----------------互動頁無動作server丟值
     if request.method =='GET':
         
@@ -207,6 +208,25 @@ def test_recommand():
         return render_template('about.html',username=session['username'],favorite_exinfo=favorite_exinfo)
     
     
+=======
+    min_county = PM25().Get_min_county()
+    pm,_ = PM25().Get_one_PM25(min_county)
+    recommand_county = exinfo().Get_county_exinfo(min_county)
+
+    return render_template('recommand.html',recommand,pm)
+def Post_user_favorite():
+     if request.method =='POST':
+        username = request.form['username']
+
+        recommand = request.form.getlist('exinfo')
+        sql = "INSERT INTO RECOMMAND VALUE(%(USER)s,%(EXINFO)s)"
+        DoSQL.IUD_db(sql,username,recommand,2)
+
+
+    
+     return render_template('exInfo.html',pm=pm,recommand_county=recommand_county)
+
+>>>>>>> parent of f7fffa4... 推薦checkbox 傳值 and insert table 完成
 @app.route('/user_private')
 @is_logged_in
 def user_private():
