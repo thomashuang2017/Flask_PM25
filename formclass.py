c2b5d52bb1c68c2603b20bd06132ef581e189f10
MproKai <willyyoungman@gmail.com>
2018-12-26 13:26:07 +0800

    register font
    stupid way to change font style

--- formclass.py
+++ formclass.py
@@ -10,18 +10,16 @@ from wtforms import Form,StringField,TextAreaField,PasswordField,validators
 # 註冊的 form class
 class RegisterForm(Form):
     
-    username = StringField('Username',
+    username = StringField('USERNAME',
                            [validators.Regexp("^[A-Za-z0-9]*$",message="Username must contain only letters and number"),
                            validators.Length(min=3,max=15)])
     
-    email = StringField('Email',[validators.Length(min=6,max=50)])
-    password = PasswordField('Password',[
+    email = StringField('EMAIL',[validators.Length(min=6,max=50)])
+    password = PasswordField('PASSWORD',[
             validators.Regexp("^[A-Za-z0-9]*$",message="Password must contain only letters and number"),
             validators.Length(min=1,max=10),
             validators.DataRequired(),
-            validators.EqualTo('confirm',message = 'Passwords do not match')
+            validators.EqualTo('CONFIRM',message = 'Passwords do not match')
     ])
-    confirm = PasswordField('Confirm Password')
+    confirm = PasswordField('CONFIRM PASSWORD')
     
-
-        
